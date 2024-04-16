b7ae79d1b9bce7685a5372d58b06c867174b0a5b
Neal Rodriguez <neal.rodriguez@obf.ateneo.edu>
2024-04-17 01:20:14 +0800

    added workout types 3, 6 and 7 to the addform

--- intellifit/src/component-pages/WorkoutsAdd.tsx
+++ intellifit/src/component-pages/WorkoutsAdd.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import "./WorkoutsAddPage.css";
 import Minigreeter from "../components/Minigreeter";
 import React, { FormEvent, ReactComponentElement, useEffect, useState } from 'react'
+import { breadcrumbsClasses } from "@mui/material";
 
 type workoutFields = {
     workout: number,
@@ -13,28 +14,19 @@ type workoutFields = {
     workout_type: String;
 }
 
-function getWorkout_Type(workout: number, exercise_id: number): string{
-    var workoutType: string;
-    
-    if((workout >= 1 && workout <= 3) || (workout >=6 && workout <= 7)){
-        workoutType="set-rep-weight";
-    } else if(workout==4){
-        workoutType="distance";
-    } else if (workout==5){
-        workoutType="set-rep-duration";
-    } else if (workout==8 && (exercise_id == 35 || exercise_id == 36 || exercise_id == 38)) {
-        workoutType="set-rep-duration";
-    } else {
-        workoutType="set-rep-weight";
-    }
-
-    return workoutType;
-}
-
 function WorkoutsAdd(){
-    
     const currentDate: Date = new Date();
     const formattedDate: string = currentDate.toISOString().split('T')[0];
+
+    const [values, setValues] = useState<workoutFields>({
+        workout: 0,
+        exercise: 0,
+        sets: 0,
+        reps: 0,
+        weight: 0,
+        date: formattedDate,
+        workout_type: '',
+    });
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setValues({ ...values, [event.target.name]: event.target.value });
@@ -69,18 +61,35 @@ function WorkoutsAdd(){
         .catch(error => console.error('Error fetching Push exercises:', error));
     }, []);
 
-    const [values, setValues] = useState<workoutFields>({
-        workout: 0,
-        exercise: 0,
-        sets: 0,
-        reps: 0,
-        weight: 0,
-        date: formattedDate,
-        workout_type: '',
-    });
+    const [exercises_3, setExercises_3] = useState<any[]>([])
+    useEffect(() => {
+        fetch('http://localhost:3003/ex_entries-3')
+        .then(res => res.json())
+        .then(data => setExercises_3(data))
+        .catch(error => console.error('Error fetching Pull exercises:', error));
+    }, []);
+
+    const [exercises_6, setExercises_6] = useState<any[]>([])
+    useEffect(() => {
+        fetch('http://localhost:3003/ex_entries-6')
+        .then(res => res.json())
+        .then(data => setExercises_6(data))
+        .catch(error => console.error('Error fetching Full Body exercises:', error));
+    }, []);
+
+    const [exercises_7, setExercises_7] = useState<any[]>([])
+    useEffect(() => {
+        fetch('http://localhost:3003/ex_entries-7')
+        .then(res => res.json())
+        .then(data => setExercises_7(data))
+        .catch(error => console.error('Error fetching Core exercises:', error));
+    }, []);
 
     const [selectEx1, setEx1] = useState<boolean>(false);
     const [selectEx2, setEx2] = useState<boolean>(false);
+    const [selectEx3, setEx3] = useState<boolean>(false);
+    const [selectEx6, setEx6] = useState<boolean>(false);
+    const [selectEx7, setEx7] = useState<boolean>(false);
     
     const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         const chosenWorkout = event.target.value;
@@ -88,14 +97,44 @@ function WorkoutsAdd(){
             case "1":
                 setEx1(true);
                 setEx2(false);
+                setEx3(false);
+                setEx6(false);
+                setEx7(false);
                 break;
             case "2":
                 setEx1(false);
                 setEx2(true);
+                setEx3(false);
+                setEx6(false);
+                setEx7(false);
+                break;
+            case "3":
+                setEx1(false);
+                setEx2(false);
+                setEx3(true);
+                setEx6(false);
+                setEx7(false);
+                break;
+            case "6":
+                setEx1(false);
+                setEx2(false);
+                setEx3(false);
+                setEx6(true);
+                setEx7(false);
+                break;
+            case "7":
+                setEx1(false);
+                setEx2(false);
+                setEx3(false);
+                setEx6(false);
+                setEx7(true);
                 break;
             default:
                 setEx1(false);
                 setEx2(false);
+                setEx3(false);
+                setEx6(false);
+                setEx7(false);
         }
     };
 
@@ -154,22 +193,95 @@ function WorkoutsAdd(){
                     )}
                     {selectEx2 && (
                         <div className="exerSelect">
-                        <p>Choose Exercise: </p>
-                        <select id="exercise">
-                            <option value="">Choose Exercise</option>
-                            {exercises_2.map(exercise_2 => (
-                                <option key={exercise_2.exercise_id} value={exercise_2.exercise_id}>{exercise_2.exercise_name}</option>
-                            ))}
-                        </select>
-                        <div className="sets">
+                            <p>Choose Exercise: </p>
+                            <select id="exercise" name="exercise" onChange={handleExerciseChange}>
+                                <option value="">Choose Exercise</option>
+                                {exercises_2.map(exercise_2 => (
+                                    <option key={exercise_2.exercise_id} value={exercise_2.exercise_id}>{exercise_2.exercise_name}</option>
+                                ))}
+                            </select>
+                            <div className="sets">
                                 <p>Enter Sets: </p>
                                 <input type='number'placeholder="sets" name="sets" onChange={handleChange}/>
-                        </div>
-                        <div className="reps">
+                            </div>
+                            <div className="reps">
                                 <p>Enter Reps: </p>
                                 <input type='number' placeholder="reps" name="reps" onChange={handleChange}/>
+                            </div>
+                            <div className="weight">
+                                <p>Enter Weight: </p>
+                                <input type='number' placeholder="weight" name="weight" onChange={handleChange}/>
+                            </div>
                         </div>
-                    </div>
+                    )}
+                    {selectEx3 && (
+                        <div className="exerSelect">
+                            <p>Choose Exercise: </p>
+                            <select id="exercise" name="exercise" onChange={handleExerciseChange}>
+                                <option value="">Choose Exercise</option>
+                                {exercises_3.map(exercise_3 => (
+                                    <option key={exercise_3.exercise_id} value={exercise_3.exercise_id}>{exercise_3.exercise_name}</option>
+                                ))}
+                            </select>
+                            <div className="sets">
+                                <p>Enter Sets: </p>
+                                <input type='number'placeholder="sets" name="sets" onChange={handleChange}/>
+                            </div>
+                            <div className="reps">
+                                <p>Enter Reps: </p>
+                                <input type='number' placeholder="reps" name="reps" onChange={handleChange}/>
+                            </div>
+                            <div className="weight">
+                                <p>Enter Weight: </p>
+                                <input type='number' placeholder="weight" name="weight" onChange={handleChange}/>
+                            </div>
+                        </div>
+                    )}
+                    {selectEx6 && (
+                        <div className="exerSelect">
+                            <p>Choose Exercise: </p>
+                            <select id="exercise" name="exercise" onChange={handleExerciseChange}>
+                                <option value="">Choose Exercise</option>
+                                {exercises_6.map(exercise_6 => (
+                                    <option key={exercise_6.exercise_id} value={exercise_6.exercise_id}>{exercise_6.exercise_name}</option>
+                                ))}
+                            </select>
+                            <div className="sets">
+                                <p>Enter Sets: </p>
+                                <input type='number'placeholder="sets" name="sets" onChange={handleChange}/>
+                            </div>
+                            <div className="reps">
+                                <p>Enter Reps: </p>
+                                <input type='number' placeholder="reps" name="reps" onChange={handleChange}/>
+                            </div>
+                            <div className="weight">
+                                <p>Enter Weight: </p>
+                                <input type='number' placeholder="weight" name="weight" onChange={handleChange}/>
+                            </div>
+                        </div>
+                    )}
+                    {selectEx7 && (
+                        <div className="exerSelect">
+                            <p>Choose Exercise: </p>
+                            <select id="exercise" name="exercise" onChange={handleExerciseChange}>
+                                <option value="">Choose Exercise</option>
+                                {exercises_7.map(exercise_7 => (
+                                    <option key={exercise_7.exercise_id} value={exercise_7.exercise_id}>{exercise_7.exercise_name}</option>
+                                ))}
+                            </select>
+                            <div className="sets">
+                                <p>Enter Sets: </p>
+                                <input type='number'placeholder="sets" name="sets" onChange={handleChange}/>
+                            </div>
+                            <div className="reps">
+                                <p>Enter Reps: </p>
+                                <input type='number' placeholder="reps" name="reps" onChange={handleChange}/>
+                            </div>
+                            <div className="weight">
+                                <p>Enter Weight: </p>
+                                <input type='number' placeholder="weight" name="weight" onChange={handleChange}/>
+                            </div>
+                        </div>
                     )}
                     <button id="Add-Workout">Add Workout</button>
                 </form>
