dbe646d815af13e3afa63709f931cb4155cd6c9a
Nicholo Pardines <nick123abl@gmail.com>
2024-04-21 01:56:57 +0800

    TODO: Finish home and restructure db to use created_by

--- intellifit/src/component-pages/Homepage.tsx
+++ intellifit/src/component-pages/Homepage.tsx
@@ -1,11 +1,9 @@
-
 import Minigreeter from "../components/Minigreeter";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "./auth-pages/AuthContext";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
-
 
 type user = {
   user_id: number;
@@ -32,12 +30,12 @@ function Home() {
   };
 
   const newData = {
-    calorie_goal : calorie,
+    calorie_goal: calorie,
     protein_goal: protein,
-    weight_kg : weight,
-    height_cm : height,
-    username : loggedInUser
-  }
+    weight_kg: weight,
+    height_cm: height,
+    username: loggedInUser,
+  };
   useEffect(() => {
     axios
       .post("http://localhost:3003/userinfo", initialData)
@@ -48,7 +46,7 @@ function Home() {
         setCalorie(response.data[0].calorie_goal); // Directly use response.data to set calorie
         setHeight(response.data[0].height_cm); // Directly use response.data to set height
         setProtein(response.data[0].protein_goal); // Directly use response.data to set protein
-        setWeight(response.data[0].weight_kg); 
+        setWeight(response.data[0].weight_kg);
         console.log(weight);
       })
       .catch((error) => {
@@ -58,7 +56,7 @@ function Home() {
 
   return (
     <>
-    {/* <p>{calorie && <>{calorie}</>}</p> */}
+      {/* <p>{calorie && <>{calorie}</>}</p> */}
       <Minigreeter label="Home Dashboard: "></Minigreeter>
       <div className="meal-option-flex-container">
         <button
@@ -90,104 +88,230 @@ function Home() {
           </div>
           <div className="one-item-card">
             <h1>Goals: </h1>
-            {editSettings && <>
-              <div
-              className="horizontal-flex"
-              style={{ alignItems: "center", justifyContent: "space-between", height: "50px", fontSize:"20px" }}
-            >
-              <p style={{ margin: "0px",fontSize:"18px" }}>Calorie Goal: </p>{" "}
-              <input type="number" value={calorie} onChange={(e)=>{setCalorie(e.target.value)}}/>
-            </div>
-            <div
-              className="horizontal-flex"
-              style={{ alignItems: "center", justifyContent: "space-between", height: "50px", fontSize:"20px" }}
-            >
-              <p style={{ margin: "0px",fontSize:"18px" }}>Protein Goal (grams): </p>{" "}
-              <input type="number" value={protein} onChange={(e)=>{setProtein(e.target.value)}} />
-            </div>
-            <div
-              className="horizontal-flex"
-              style={{ alignItems: "center", justifyContent: "space-between", height: "50px", fontSize:"20px" }}
-            >
-              <p style={{ margin: "0px",fontSize:"18px" }}> Weight (kg): </p>{" "}
-              <input type="number" value={weight} onChange={(e)=>{setWeight(e.target.value)}}/>
-            </div>
-            <div
-              className="horizontal-flex"
-              style={{ alignItems: "center", justifyContent: "space-between", height: "50px", fontSize:"20px" }}
-            >
-              <p style={{ margin: "0px",fontSize:"18px" }} > Height (cm): </p>{" "}
-              <input type="number"  onChange={(e)=>{setHeight(e.target.value)}} value={height}/>
-            </div>
-            <button className="meal-option" onClick={()=>{
-              setEditSettings(false)
-              axios.post("http://localhost:3003/userinfo/update",newData)
-              .then((response)=>{
-                console.log(response.data);
-              })
-              .catch((error)=>{
-                console.log(error.response.data);
-              });
-            }}>Save</button>
-            </>}
+            {editSettings && (
+              <>
+                <div
+                  className="horizontal-flex"
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    height: "50px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <p style={{ margin: "0px", fontSize: "18px" }}>
+                    Calorie Goal:{" "}
+                  </p>{" "}
+                  <input
+                    type="number"
+                    value={calorie}
+                    onChange={(e) => {
+                      setCalorie(e.target.value);
+                    }}
+                  />
+                </div>
+                <div
+                  className="horizontal-flex"
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    height: "50px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <p style={{ margin: "0px", fontSize: "18px" }}>
+                    Protein Goal (grams):{" "}
+                  </p>{" "}
+                  <input
+                    type="number"
+                    value={protein}
+                    onChange={(e) => {
+                      setProtein(e.target.value);
+                    }}
+                  />
+                </div>
+                <div
+                  className="horizontal-flex"
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    height: "50px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <p style={{ margin: "0px", fontSize: "18px" }}>
+                    {" "}
+                    Weight (kg):{" "}
+                  </p>{" "}
+                  <input
+                    type="number"
+                    value={weight}
+                    onChange={(e) => {
+                      setWeight(e.target.value);
+                    }}
+                  />
+                </div>
+                <div
+                  className="horizontal-flex"
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    height: "50px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <p style={{ margin: "0px", fontSize: "18px" }}>
+                    {" "}
+                    Height (cm):{" "}
+                  </p>{" "}
+                  <input
+                    type="number"
+                    onChange={(e) => {
+                      setHeight(e.target.value);
+                    }}
+                    value={height}
+                  />
+                </div>
+                <button
+                  className="meal-option"
+                  onClick={() => {
+                    setEditSettings(false);
+                    axios
+                      .post("http://localhost:3003/userinfo/update", newData)
+                      .then((response) => {
+                        console.log(response.data);
+                      })
+                      .catch((error) => {
+                        console.log(error.response.data);
+                      });
+                  }}
+                >
+                  Save
+                </button>
+              </>
+            )}
 
-
-            {!editSettings && userData  && <>
-              <div
-              className="horizontal-flex"
-              style={{ alignItems: "center", justifyContent: "space-between", height: "30px", fontSize:"20px" }}
-            >
-              <p style={{ margin: "0px" }}>Calorie Goal: </p>{" "}
-              <span>
-                {calorie ? calorie: "Not Set"}
-              </span>
-            </div>
-            <div
-              className="horizontal-flex"
-              style={{ alignItems: "center", justifyContent: "space-between", height: "30px", fontSize:"20px" }}
-            >
-              <p style={{ margin: "0px" }}>Protein Goal (grams): </p>{" "}
-              <span>
-                {protein == null
-                  ? "Not Set"
-                  : protein}
-              </span>
-            </div>
-            <div
-              className="horizontal-flex"
-              style={{ alignItems: "center", justifyContent: "space-between", height: "30px", fontSize:"20px" }}
-            >
-              <p style={{ margin: "0px" }}> Weight (kg): </p>{" "}
-              <span>
-                {weight == null
-                  ? "Not Set"
-                  : weight}
-              </span>
-            </div>
-            <div
-              className="horizontal-flex"
-              style={{ alignItems: "center", justifyContent: "space-between", height: "30px", fontSize:"20px" }}
-            >
-              <p style={{ margin: "0px" }}> Height (cm): </p>{" "}
-              <span>
-                {height == null
-                  ? "Not Set"
-                  : height}
-              </span>
-            </div>
-            <button className="meal-option" onClick={() => {setEditSettings(true)}}>
-              Edit
-            </button>
-            </>}
-
-           
+            {!editSettings && userData && (
+              <>
+                <div
+                  className="horizontal-flex"
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    height: "30px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <p style={{ margin: "0px" }}>Calorie Goal: </p>{" "}
+                  <span>{calorie ? calorie : "Not Set"}</span>
+                </div>
+                <div
+                  className="horizontal-flex"
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    height: "30px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <p style={{ margin: "0px" }}>Protein Goal (grams): </p>{" "}
+                  <span>{protein == null ? "Not Set" : protein}</span>
+                </div>
+                <div
+                  className="horizontal-flex"
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    height: "30px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <p style={{ margin: "0px" }}> Weight (kg): </p>{" "}
+                  <span>{weight == null ? "Not Set" : weight}</span>
+                </div>
+                <div
+                  className="horizontal-flex"
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    height: "30px",
+                    fontSize: "20px",
+                  }}
+                >
+                  <p style={{ margin: "0px" }}> Height (cm): </p>{" "}
+                  <span>{height == null ? "Not Set" : height}</span>
+                </div>
+                <button
+                  className="meal-option"
+                  onClick={() => {
+                    setEditSettings(true);
+                  }}
+                >
+                  Edit
+                </button>
+              </>
+            )}
           </div>
         </>
       )}
       {!toggleSettings && (
         <>
-          <div className="user-info">
-            <h1 style={{ marginLeft: "10px" }}>Nutrition Stats: </h1>
+          <div className="one-item-card">
+            <div className="user-info">
+              <h1 style={{ marginLeft: "10px" }}>Nutrition Stats (Today): </h1>
+              {!editSettings && userData && (
+                <>
+                  <div
+                    className="horizontal-flex"
+                    style={{
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      height: "30px",
+                      fontSize: "20px",
+                    }}
+                  >
+                    <p style={{ margin: "0px" }}>Calorie Goal: </p>{" "}
+                    <span>{calorie ? calorie : "Not Set"}</span>
+                  </div>
+                  <div
+                    className="horizontal-flex"
+                    style={{
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      height: "30px",
+                      fontSize: "20px",
+                    }}
+                  >
+                    <p style={{ margin: "0px" }}>Protein Goal (grams): </p>{" "}
+                    <span>{protein == null ? "Not Set" : protein}</span>
+                  </div>
+                  <div
+                    className="horizontal-flex"
+                    style={{
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      height: "30px",
+                      fontSize: "20px",
+                    }}
+                  >
+                    <p style={{ margin: "0px" }}> Weight (kg): </p>{" "}
+                    <span>{weight == null ? "Not Set" : weight}</span>
+                  </div>
+                  <div
+                    className="horizontal-flex"
+                    style={{
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      height: "30px",
+                      fontSize: "20px",
+                    }}
+                  >
+                    <p style={{ margin: "0px" }}> Height (cm): </p>{" "}
+                    <span>{height == null ? "Not Set" : height}</span>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </>
       )}
