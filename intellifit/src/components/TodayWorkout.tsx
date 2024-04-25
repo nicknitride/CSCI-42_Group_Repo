e6f58a05dd349470a089d5b84bb9c28b91be7018
nealrodriguez <neal.rodriguez@student.ateneo.edu>
2024-04-25 20:51:02 +0800

    Revert "removed comments"
    This reverts commit 722a01ecc0fbcd25bcb9d207eb4aee15f514273e.

--- intellifit/src/components/TodayWorkout.tsx
+++ intellifit/src/components/TodayWorkout.tsx
@@ -25,7 +25,60 @@ function TodayWorkout({data}: props){
                         </div>
                     </ul>
                 </div>
+                {/*<div className="workout-card">
+                    <h2>March 22, 2024</h2>
+                    <p className="workout-name">Push Workout</p>
+                    <div className="duration">
+                        <span>Start: <span className="sets-value">1:25 PM</span></span>
+                        <span>End: <span className="reps-value">2:00 PM</span></span>
+                    </div>
+                    <h3>Exercises</h3>
+                    <ul className="exercise-list">
+                        <div className="list-detail">
+                            <li>Incline Chest Press</li>
+                            <li>3 Sets</li>
+                            <li>12 Reps</li>
+                        </div>
+                        <div className="list-detail">
+                            <li>Pec Fly</li>
+                            <li>3 Sets</li>
+                            <li>12 Reps</li>
+                        </div>
+                        <div className="list-detail">
+                            <li>Shoulder Press</li>
+                            <li>3 Sets</li>
+                            <li>12 Reps</li>
+                        </div>
+                    </ul>
+                </div>*/}
+                {/*<div className="workout-card">
+                    <h2>March 20, 2024</h2>
+                    <p className="workout-name">Pull Workout</p>
+                    <div className="duration">
+                        <span>Start: <span className="sets-value">9:00 PM</span></span>
+                        <span>End: <span className="reps-value">10:00 PM</span></span>
+                    </div>
+                    <h3>Exercises</h3>
+                    <ul className="exercise-list">
+                        <div className="list-detail">
+                            <li>Lat Pulldown</li>
+                            <li>3 Sets</li>
+                            <li>12 Reps</li>
+                        </div>
+                        <div className="list-detail">
+                            <li>Rear Delt Machine</li>
+                            <li>3 Sets</li>
+                            <li>12 Reps</li>
+                        </div>
+                        <div className="list-detail">
+                            <li>Cable Curls</li>
+                            <li>3 Sets</li>
+                            <li>12 Reps</li>
+                        </div>
+    </ul>
+    </div>*/}
             </div>
+            
         </>
     )
 };
