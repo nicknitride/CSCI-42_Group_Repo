2067137ccb81bff7a94640723235228c81fad176
nealrodriguez <neal.rodriguez@student.ateneo.edu>
2024-04-26 13:58:34 +0800

    NavBar and Exercise display are mobile responsive

--- intellifit/src/components/Navbar.tsx
+++ intellifit/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import "../css/Navbar.css";
+/*import "../css/Navbar.css";
 import gymlogo from "../assets/1_gym.svg";
 import { Link } from "react-router-dom";
 function Navbar() {
@@ -27,4 +27,53 @@ function Navbar() {
     </>
   );
 }
+export default Navbar;*/
+import React, { useState } from 'react';
+import "../css/Navbar.css";
+import gymlogo from "../assets/1_gym.svg";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  return (
+    <>
+      <div className="outer-nav-flex">
+        <div className="left-nav-flex">
+          <div className="rounded-button">
+            <Link to="/">Home</Link>
+          </div>
+          <div className="rounded-button">
+            <Link to="/workouts">Workouts</Link>
+          </div>
+          <div className="rounded-button">
+            <Link to="/meals">Meal Tracking</Link>
+          </div>
+        </div>
+        <div className="nav-logo">
+          <img src={gymlogo} alt="Gym Logo" />
+          <span>Intellifit {String.fromCharCode(8482)}</span>
+        </div>
+        
+        {/* Mobile dropdown */}
+        <div className="dropdown-mobile" onClick={toggleDropdown}>
+          <img src={gymlogo} alt="Gym Logo" />
+          {showDropdown && (
+            <div className="content">
+              <Link to="/">Home</Link>
+              <Link to= "/workouts">Workouts</Link>
+              <Link to="/meals">Meal Tracking</Link>
+            </div>
+          )}
+        </div>
+
+      </div>
+    </>
+  );
+}
+
 export default Navbar;
